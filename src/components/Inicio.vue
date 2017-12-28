bc4024fdf078f48e4c621ca1de57e1b78e3697ca
juany-ramirez <juany.ramrz@unitec.edu>
2017-12-27 20:49:03 -0600

    Beneficios, Valores, Sobre nosotros, proyectos

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -1,40 +1,33 @@
 <template>
   <div class="container-fluid" id="inicio">
+    <!--  Frase Principal-->
     <div class="fixed-bg center" id="trees">
-      <br>
-      <br>
-      <p>Somos una cooperativa orgullosamente hondureña, creada por un grupo de ingenieros agrónomos con el objetivo de generar bienestar socioeconomico,
-        fomentar el emprendimiento y las buenas prácticas agropecuarias entre sus miembros y la comunidad que actúa. Actualmente, cuenta con profesionales de diversas áreas
-        siendo el capital humano su riqueza más valiosa.
-      </p>
-      <br>
-      <br>
-      <br>
-
+      Apoyamos a todos aquellos que con
+      esfuerzo y sacrificio dan fertilidad a
+      nuestras tierras catrachas, con el
+      compromiso que nos caracteriza.
+    </div>
+    <!--  VIRTUDES-->
+    <div class="-long3 -lightgreen" id="virtudes">
       <div id="icon_container"class="row">
-        <div class="col s4  -center">
+        <div class="col s6 m4  -center">
           <i class="large material-icons">directions_walk</i>
           <br>
           TRABAJO
         </div>
-        <div class="col s4  -center">
+        <div class="col s6 m4  -center">
           <i class="large material-icons">trending_up</i>
           <br>
           DESARROLLO
         </div>
-        <div class="col s4  -center">
+        <div class="col s12 m4  -center">
           <i class="large material-icons">accessibility</i>
           <br>
           SOLIDARIDAD
         </div>
       </div>
     </div>
-    <!-- |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||-->
-    <div class="fixed-bg -center -frase1" id="leaves">
-      Apoyamos a todos aquellos que con esfuerzo y sacrificio <br>
-      dan fertilidad a nuestras tierras catrachas,  <br>
-      con el compromiso que nos caracteriza.
-    </div>
+    <!--  ¿Qué es COMDAHL?-->
     <div class="row center" >
       <div class="col s6 -white -long2 -right" id="contenedor-principal">
         <div class="typewriter">
@@ -51,12 +44,12 @@
         </p>
       </div>
     </div>
-
+    <!--  Mision Vision-->
     <div class="row">
       <div class="col m6 -lightgreen -long2 " id="contenedorVMision">
-        <i class="large material-icons">settings</i>
+
         <div >
-          <h1 id="MVision">MISIÓN</h1>
+          <h1 id="MVision"><i class="medium material-icons">settings</i> MISIÓN</h1>
         </div>
         <p>
           Somos una empresa cooperativa de primer grado, que promueve
@@ -71,9 +64,8 @@
       <div class="col m6 -green -long2  " id="contenedorVMision">
 
         <div >
-          <i class="large material-icons">remove_red_eye</i>
-          <br>
-          <h1 id="MVision">VISIÓN</h1>
+
+          <h1 id="MVision"><i class="medium material-icons">remove_red_eye</i>VISIÓN</h1>
         </div>
         <div>
           <p>
@@ -86,53 +78,167 @@
             crecimiento de la economía social en el país.
           </p>
         </div>
-
-
       </div>
     </div>
-    <div class="fixed-bg" id="pattern">
-      <div class="row" id="proyectos">
-        <div class="col m3 -long1 color1" id="proyecto1">
-          <div class="flip-container" ontouchstart="this.classList.toggle('hover');">
-            <div class="flipper">
-              <div class="front">
-                <!-- front content -->
-              </div>
-              <div class="back">
-                <!-- back content -->
-                <p>asdahsihasughasfasgasapisughasghaposghpasigpaohsguihaspg
-                gasiguhasiughasiguhao´sghaioshgiopashgoáishgoiahsgiphasghiag
-              asgashgiuashgioahsgiohasioghaoighasoih</p>
-              </div>
-            </div>
+    <!--  Valores-->
+    <div class="-long4 center -blue">
+      <div class="flow-text"id="valores">
+        <h1>VALORES COORPORATIVOS</h1>
+        <div class="container row">
+          <div class="col s6 m4 -center -long1 -yellow" id="contenedorValor">
+            Igualdad
+          </div>
+          <div class="col s6 m4 -center -long1 -lightgreen" id="contenedorValor">
+            Ayuda Mutua
+          </div>
+          <div class="col s6 m4 -center -long1 -green" id="contenedorValor">
+            Respeto
+          </div>
+          <div class="col s6 m4 -center -long1 -green" id="contenedorValor">
+            Compromiso
+          </div>
+          <div class="col s6 m4 -center -long1 -yellow" id="contenedorValor">
+            Confianza
+          </div>
+          <div class="col s6 m4 -center -long1 -lightgreen" id="contenedorValor">
+            Honestidad
           </div>
         </div>
-        <div class="col m3 -long1 color2" id="proyecto2">
+      </div>
+    </div>
+    <!--  Frase secundaria-->
+    <div class="container" id="frase3">
+      <p>Somos una cooperativa orgullosamente hondureña, creada por un grupo de ingenieros agrónomos con el objetivo de generar bienestar socioeconomico,
+        fomentar el emprendimiento y las buenas prácticas agropecuarias entre sus miembros y la comunidad que actúa. Actualmente, cuenta con profesionales de diversas áreas
+        siendo el capital humano su riqueza más valiosa.
+      </p>
+    </div>
+    <!--  Imagen-->
+    <div class="fixed-bg -center -long3 -frase1 -blue" id="leaves">
+    </div>
+    <!--  Proyectos-->
+    <div class="-long4 center -white">
+      <div class="flow-text"id="valores">
+        <br>
+        <h1>Proyectos</h1>
+        <br>
+        <div class="container">
+          <div class="slider">
+            <ul class="slides">
+              <li>
+                <img src="..\dist\Desarrollo comunal.jpg">
+                <div class="caption center-align">
+                  <h3>Desarrollo comunal localizado en aldea el limón, Cerro de Hula </h3>
+                </div>
+              </li>
+              <li>
+                <img src="../dist/Agropecuaria.jpg">
+                <div class="caption left-align">
+                  <h3>Agropecuaria Comercial COMDAHL </h3>
 
-        </div>
-        <div class="col m3 -long1 color1" id="proyecto3">
+                </div>
+              </li>
+              <li>
+                <img src="../dist/Veterinario.jpg">
+                <div class="caption right-align">
+                  <h3>Laboratorio Veterinario COMDAHL</h3>
 
-        </div>
-        <div class="col m3 -long1 color2" id="proyecto4">
+                </div>
+              </li>
+              <li>
+                <img src="..\dist\Fabrica de biofertilizantes.jpg">
+                <div class="caption center-align">
+                  <h3>Fábrica de biofertilizantes y biopesticidas formulados y elaborados en el país</h3>
 
+                </div>
+              </li>
+              <li>
+                <img src="..\dist\areas Protegidas.jpg">
+                <div class="caption center-align">
+                  <h3>Proyecto Alfa y Omega COMDAHL (Protección de Áreas Protegidas Privadas)</h3>
+
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-
-
+    </div>
+    <!--  Requisitos-->
+    <div class="-long2 center -black" id="requisitos">
+      <br><br><br>
+      <h1>Requisitos</h1>
       <ul class="collapsible popout" data-collapsible="accordion">
         <li>
-          <div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div>
-          <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+          <div class="collapsible-header"><i class="material-icons">filter_drama</i>Podrán ser parte de COMDAHL:</div>
+          <div class="collapsible-body"><span>1)	los estudiantes y/o egresados mayores de 16 años de las carreras de las Ciencias Agrícolas o Carreras afines como ser: (Ingeniería forestal, Economía agrícola, administración agropecuaria, licenciatura en turismo, Ingeniería ambiental, etc.), con pleno goce de sus derechos ciudadanos.
+          2)	Personas auspiciadas o recomendadas por una persona que ya es miembro de COMDAHL.
+          3)	Empresas o instituciones con personería jurídica en economía social y solidaria.</span></div>
         </li>
         <li>
-          <div class="collapsible-header"><i class="material-icons">place</i>Second</div>
-          <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+          <div class="collapsible-header"><i class="material-icons">place</i>Para poder afiliarse una persona natural deberá presentar:</div>
+          <div class="collapsible-body"><span>1)	Por escrito solicitud de ingreso dirigida a la junta directiva;
+          2)	Presentar sus documentos personales;
+          3)	Pagar una cuota de ingreso no rembolsable de L.2,000.00 anuales;
+          4)	Presentar declaración de beneficiarios (as).</span></div>
         </li>
         <li>
-          <div class="collapsible-header"><i class="material-icons">whatshot</i>Third</div>
-          <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+          <div class="collapsible-header"><i class="material-icons">whatshot</i> Para poder afiliarse las empresas o instituciones con personería jurídica deberán presentar:</div>
+          <div class="collapsible-body"><span>1)	Personería jurídica;
+          2)	Estatuto Social o documento equivalente;
+          3)	Certificación del acta de Junta Directiva en donde conste la decisión             de afiliarse a la cooperativa;
+          4)	Certificación del punto de acta de la Junta Directiva que acredite la persona que ostenta la representación legal de conformidad a su estatuto;
+          5)	Constancia de inscripción de la junta directiva en la unidad de Registro               y Seguimiento de Sociedades Civiles (URSAC) o su equivalente;
+          6)	Documentos personales del representante legal¬.</span></div>
         </li>
       </ul>
+    </div>
+
+
+
+    <!--  Beneficios-->
+    <div class="fixed-bg -long4 -frase1 center" id="blacky">
+      <div class="flow-text"id="valores">
+        <h1><i class="medium material-icons">star</i>BENEFICIOS</h1>
+        <br>
+        <div class="container row">
+          <div class="col s6 m4 -center -long6" id="beneficios">
+            <h3>1.</h3>
+            <br>
+            	Seguro de Vida y Gastos fúnebres***
+          </div>
+          <div class="col s6 m4 -center -long6" id="beneficios">
+            <h3>2.</h3>
+            <br>
+            Ahorro con tasas de interés competitivas;
+          </div>
+          <div class="col s6 m4 -center -long6" id="beneficios">
+            <h3>3.</h3>
+            <br>
+            Participación Democrática dentro de las asambleas.
+          </div>
+          <div class="col s6 m4 -center -long6" id="beneficios">
+            <h3>4.</h3>
+            <br>
+            Acceso a buro de Becas;
+          </div>
+          <div class="col s6 m4 -center -long6" id="beneficios">
+            <h3>5.</h3>
+            <br>
+            Acceso buro de Empleo;
+          </div>
+          <div class="col s6 m4 -center -long6" id="beneficios">
+            <h3>6.</h3>
+            <br>
+            Capacitaciones formativas en diversas áreas;
+          </div>
+          <div class="col s12 m12 -center -long6" id="beneficios">
+            <h3>7.</h3>
+            <br>
+            Prestamos con bajas tasas de interés (Préstamos Automáticos, Préstamos para producción agropecuaria, Préstamos fiduciarios, Préstamos hipotecarios, y, Préstamos Prendarios);
+          </div>
+        </div>
+      </div>
     </div>
 
   </div>
@@ -145,6 +251,11 @@ export default {
     $(document).ready(function(){
       $('.collapsible').collapsible();
     });
+
+    $(document).ready(function(){
+      $('.slider').slider();
+    });
+
   }
 }
 
@@ -156,15 +267,13 @@ export default {
 	perspective: 1000px;
 }
 	/* flip the pane when hovered */
-	.flip-container:hover .flipper, .flip-container.hover .flipper {
+.flip-container:hover .flipper, .flip-container.hover .flipper {
 		transform: rotateY(180deg);
 	}
-
 .flip-container, .front, .back {
 	width: 100%;
 	height: 280px;
 }
-
 /* flip speed goes here */
 .flipper {
 	transition: 0.6s;
@@ -172,7 +281,6 @@ export default {
 
 	position: relative;
 }
-
 /* hide back of pane during swap */
 .front, .back {
 	backface-visibility: hidden;
@@ -181,14 +289,12 @@ export default {
 	left: 0;
   background-image: url('../imgs/Agropecuaria.jpg');
 }
-
 /* front pane, placed above back */
 .front {
 	z-index: 2;
 	/* for firefox 31 */
 	transform: rotateY(0deg);
 }
-
 /* back, initially hidden pane */
 .back {
 	transform: rotateY(180deg);
@@ -228,18 +334,69 @@ div.valores{
   position: relative;
   left: 5%;
 }
+
+#virtudes {
+  padding-top: 2%;
+  font-size: 30px;
+}
 #trees{
   background-image: url('../imgs/fondo.JPG');
   background-position: bottom;
+  height: 600px;
   min-height: 80%;
-  font-family: 'Roboto', sans-serif;
-  font-size: 175%;
+  color: #FFFFC4;
+  font-family: 'Montserrat', sans-serif !important;
+  font-weight: 600 !important;
+  font-size: 70px !important;
+  line-height: 1.2em;
   padding-top: 5%;
-  padding-left: 22%;
-  padding-right: 22%;
+  padding-left: 3%;
+  padding-right: 3%;
   line-height: 115%;
   overflow: hidden;
   width: auto;
+  -webkit-filter: saturate(150%); /* Safari */
+  filter: saturate(150%);
+}
+@-webkit-keyframes filter-animation {
+  0% {
+     -webkit-filter: sepia(0) saturate(2);
+   }
+   50% {
+     -webkit-filter: sepia(1) saturate(8);
+   }
+   100% {
+     -webkit-filter: sepia(0) saturate(2);
+   }
+}
+#valores h1{
+  font-family: 'Montserrat Subrayada', sans-serif;
+  font-size: 70px;
+  color: #CCCC52!important;
+}
+#valores{
+  padding-top: 2%;
+}
+#contenedorValor{
+  -webkit-clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+  clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+  font-family: 'Montez', cursive;
+  font-size: 40px;
+  color: #192B33!important;
+}
+#leaves{
+  background-image: url('../imgs/elotes.JPG');
+  -webkit-animation: filter-animation 5s infinite;
+  color: #212121 !important;
+}
+#frase3{
+  color: black!important;
+  font-weight: bold;
+  font-family: 'Montserrat', sans-serif;
+  font-size: 30px;
+  color: #FFFFC4 !important;
+  /*background-color: #212121 !important;*/
+
 }
 .color2{
   background: yellow;
@@ -252,45 +409,19 @@ div.valores{
   filter: grayscale(100%);
   opacity: 0.5;
 }
+#blacky{
+  background-image: url('../imgs/background.jpg');
+}
+#beneficios{
+  font-family: 'Montserrat', sans-serif;
+}
+#beneficios h3{
+  color: #CCCC52;
+}
+#requisitos ul{
+  color: black;
+}
 
-#proyecto1{
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-}
-#proyecto2{
-  /*background-image: url("../imgs/Desarrollo comunal.jpg");*/
-  background: url("../dist/Desarrollo comunal.jpg") no-repeat center center;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-  -webkit-filter: grayscale(100%);
-  filter: grayscale(100%);
-  opacity: 0.5;
-}
-#proyecto3{
-  /*background-image: url("../imgs/Veterinario.jpg");*/
-  background: url("../dist/Veterinario.jpg") no-repeat center center;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-  -webkit-filter: grayscale(100%);
-  filter: grayscale(100%);
-  opacity: 0.5;
-}
-#proyecto4{
-  /*background-image: url("../imgs/Fabrica de biofertilizantes.jpg");*/
-
-  background: url("../dist/Fabrica de biofertilizantes.jpg") no-repeat center center;
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-
-}
 .-transicion1{
   background-color: #FFD933;
   color: #FFFFC4;
@@ -303,6 +434,7 @@ div.valores{
   color: #FFFFC4;
 
 }
+
 .-center{
   display: -webkit-box;
   -webkit-box-pack: center;
@@ -324,28 +456,19 @@ div.valores{
   text-align: center;
   justify-items: start;
 }
-#trees {
-  -webkit-filter: saturate(150%); /* Safari */
-  filter: saturate(150%);
-}
+
+
 .-frase1{
-  font-family: 'Katibeh', cursive !important;
-  font-size: 60px;
+  font-family: 'Bungee', cursive;
+  line-height: 1.2em;
+  font-size: 40px;
 }
-#valores {
-  -webkit-filter: contrast(120%); /* Safari */
-  filter: contrast(120%);
-}
+
 #pattern{
   background-image: url('../imgs/image2.jpg');
   min-height: 500px;
 }
-#leaves{
-  background-image: url('https://static.pexels.com/photos/192136/pexels-photo-192136.jpeg');
-  min-height: 700px;
 
-
-}
 .fixed-bg {
   background-attachment: fixed;
   background-position: center;
@@ -360,7 +483,16 @@ div.valores{
   height: 600px;
 }
 .-long3{
-  height: 30% ;
+  height: 200px;
+}
+.-long4{
+  height: 800px;
+}
+.-long5{
+  height: 400px;
+}
+.-long6{
+  height: 120px;
 }
 #contenedor-principal {
   font-family: 'Roboto', sans-serif;
@@ -372,7 +504,7 @@ div.valores{
   padding-top: 15px;
 }
 #contenedorVMision {
-  font-size: 18px !important;
+  font-size: 1.4em;
   color: #192B33;
   padding: 90px;
   align-items: center;
@@ -380,7 +512,6 @@ div.valores{
   justify-items: center !important;
   overflow: hidden;
 }
-
 #MVision{
   align-items: center;
   text-align: center;
